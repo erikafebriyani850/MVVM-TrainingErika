bdd1ba9c07ef9b8c189afdb11ef65bb79f10b3cd
erikafebriyani850 <erikafebriyani85@gmail.com>
2020-05-28 11:08:27 +0700

    pesan

---
+++ app/src/main/java/com/erika/training/activity/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.erika.training.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.erika.training.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
